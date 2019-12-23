a63a3643a926248c8c5f3f325888f2c0b4da4a9b
Wojciech Bojarski <bojawojt@gmail.com>
2019-12-23 15:59:14 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -10,6 +10,7 @@ def naming_system
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
 
+kingdom = {}
   
   part_1 = {
     label: "Kingdom",
