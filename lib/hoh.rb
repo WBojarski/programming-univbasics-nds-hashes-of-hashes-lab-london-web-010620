a9a5f048ae39d9872581bd604c7742116e61b65e
Wojciech Bojarski <bojawojt@gmail.com>
2019-12-23 15:58:13 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -1,34 +1,82 @@
-# Build a nested HoH of the classification of biological life on Earth based
-# on these commented-out snippets assigned to PORTION_1-PORTION_4. Return
-# that HoH as the return value for the method "naming_system."
-#
-# The tests will guide your construction.
-#
-# PORTION_1 = {
-#   label: "Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class"
-#     }
-#   }
-# }
-# 
-# PORTION_2 = {
-#   label: "Order"
-# }
-# 
-# PORTION_3 = {
-#   label: "Family",
-#   sub_category: {
-#     label: "Genus",
-#   }
-# }
-# 
-# PORTION_4 = {
-#   label: "Species",
-#   sub_category: nil
-# }
+def naming_system
+  # Remember:
+  #  Kingdom
+  #  Phylum
+  #  Class
+  #  Order
+  #  Family
+  #  Genus
+  #  Species
+  # So, if we have the "Kingdom" node we should be able to "tunnel" into the
+  # HoH all the way to Species!
+
+  
+
+  #         label: "Order",
+  #         sub_category: {
+  #           label: "Family",
+  #           sub_category: {
+  #             label: "Genus",
+  #             sub_category: {
+  #               label: "Species",
+  #               sub_category: ""
+              
+  #             }
+  #           }
+  #         }
+
+  #       }
+  #     }
+
+  #   }
+  # }
+  part_1 = {
+    label: "Kingdom",
+    sub_category: {
+      label: "Phylum",
+      sub_category: {
+        label: "Class",
+        sub_category: {
+
+
+        }
+      }
+    }
+  }
+
+  part_2 = {
+  label: "Order"
+}
+
+  part_3 = {
+  label: "Family",
+  sub_category: {
+    label: "Genus",
+  }
+}
+
+  part_4 = {
+  label: "Species",
+  sub_category: nil
+}
+
+  kingdom = {
+    label: "kingdom"
+    sub_category: { part_1 
+      label: "part 1"
+      sub_category: { part_2  
+        label: "part 2"
+        sub_category: {  part_3 
+          label: "part 3"
+          sub_category: {part_4
+         } 
+        }
+      }
+   } 
+  }
+  kingdom
+end
+
 
 def naming_system
   # Remember:
